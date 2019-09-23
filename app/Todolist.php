829c75ec5081009beb68fd4e7b2d4164d1d18c60
Kamal Norizan <kamalnorizan@gmail.com>
2019-09-23 23:44:57 +0800

    update todolist model

--- app/Todolist.php
+++ app/Todolist.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Todolist extends Model
 {
     //
+    public $timestamps = true;
+
+    protected $table = 'todolists';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $fillable = ['title','Description','user_id'];
 }
