b68f0de0f036590efc0c4e272a2defbba40b777e
Kamal Norizan <kamalnorizan@gmail.com>
2019-09-24 10:35:58 +0800

    Except Destroy in Route

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('/todolist', 'TodolistController');
+Route::resource('/todolist', 'TodolistController', [
+    'except' => ['destroy']
+]);
 Route::post('/todolist/delete', 'TodolistController@delete')->name('todolist.delete');
